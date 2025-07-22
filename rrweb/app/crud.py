55d4f14fb10d659195d058a4aab21cf19e30fc96
llf <2367746876@qq.com>
2025-07-22 16:16:35 +0800

    feat: rrweb接口修改

--- rrweb/app/crud.py
+++ rrweb/app/crud.py
@@ -3,19 +3,21 @@ from . import models, schemas
 from typing import List, Optional, Tuple
 from sqlalchemy import func, and_
 
+
 def get_event(db: Session, request_id: str, company_id: int, user_id: int) -> List[models.Event]:
     return db.query(models.Event).filter(
         models.Event.request_id == request_id,
         models.Event.company_id == company_id,
         models.Event.user_id == user_id
-    ).order_by(models.Event.created_at.asc()).all()
+    ).order_by(models.Event.request_at.asc()).all()
+
 
 def get_events(db: Session, skip: int = 0, limit: int = 20) -> Tuple[int, List[models.Event]]:
     subq = db.query(
         models.Event.request_id,
         models.Event.company_id,
         models.Event.user_id,
-        func.max(models.Event.created_at).label('max_created_at')
+        func.max(models.Event.request_at).label('max_request_at')
     ).group_by(
         models.Event.request_id,
         models.Event.company_id,
@@ -28,16 +30,16 @@ def get_events(db: Session, skip: int = 0, limit: int = 20) -> Tuple[int, List[m
             models.Event.request_id == subq.c.request_id,
             models.Event.company_id == subq.c.company_id,
             models.Event.user_id == subq.c.user_id,
-            models.Event.created_at == subq.c.max_created_at
+            models.Event.request_at == subq.c.max_request_at
         )
     )
     count = query.count()
-    results = query.order_by(models.Event.created_at.desc()).offset(skip).limit(limit).all()
+    results = query.order_by(models.Event.request_at.desc()).offset(skip).limit(limit).all()
     return count, results
 
 
 def create_event(db: Session, event: schemas.EventCreate) -> models.Event:
-    db_event = models.Event(**event.dict())
+    db_event = models.Event(**event.model_dump())
     db.add(db_event)
     db.commit()
     db.refresh(db_event)
